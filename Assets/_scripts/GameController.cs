faba452d19a3d039d3bfe3c0bfe52d19a2e587ba
chicomcastro <Franciscommcastro@gmail.com>
2017-06-03 07:38:39 -0300

    Target Selection System
    Att

---
+++ Assets/_scripts/GameController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	const int FIRST = 0, SECOND = 1;
+
+	public Slider healthBar;
+	public GameObject enemyHealthBar;
+	public GameObject[] currentTarget;
+	public Material targetMaterial;
+
+	float maximumHealth = 100.0f;
+	float currentHealth;
+
+	// Use this for initialization
+	void Start () {
+		currentHealth = maximumHealth;
+		healthBar.value = SetHealth(currentHealth);
+		enemyHealthBar.SetActive (false);
+		currentTarget = new GameObject [2];
+	}
+
+	void Update () {
+		if (currentTarget [0] != null) {   // If has target someone
+			enemyHealthBar.SetActive (true);   // Active HP on top screen
+			enemyHealthBar.GetComponent<Slider> ().value = SetHealth (currentTarget [0].GetComponent<TargetSelection> ().HP);  // Shows enemy HP
+		
+			// Turns target material color into original material color and atribute it to target object
+			targetMaterial.color = currentTarget [0].GetComponent<TargetSelection> ().originalMaterial.color;
+			currentTarget [0].GetComponent<MeshRenderer> ().material = targetMaterial;
+
+			// Garants that untarget objects turn their original material back
+			if (currentTarget [1] != null) {
+				currentTarget [1].GetComponent<MeshRenderer> ().material = currentTarget [1].GetComponent<TargetSelection> ().originalMaterial;
+			}
+
+		} else {
+			enemyHealthBar.SetActive (false);
+		}
+	}
+
+	float SetHealth (float currentHealth) {
+		return currentHealth / maximumHealth;
+	}
+}
---
+++ Assets/_scripts/TargetSelection.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetSelection : MonoBehaviour {
+
+	public float HP;
+
+	public float maximumHealth = 100.0f;
+	public Material originalMaterial;
+
+	void Start () {
+		HP = maximumHealth;
+		originalMaterial = GetComponent<MeshRenderer> ().material;
+	}
+
+	void OnMouseDown ()
+	{
+		GameObject[] target = GameObject.FindGameObjectWithTag ("GameController").GetComponent<GameController> ().currentTarget;
+		GameObject aux = target[0];
+		target [0] = this.gameObject;  // Current target
+		target [1] = aux;              // Anterior target
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		HP -= 10.0f;//other.gameObject.GetComponent<SkillAttributes>.hitAmount;
+	}
+}
