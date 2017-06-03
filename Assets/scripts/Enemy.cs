1620dc6c23941a5fa30b02c737155e11f839662d
CarlosMatheus <carlosmatheusbs@gmail.com>
2017-06-03 19:16:57 -0300

    Created Enemy AI

---
+++ Assets/scripts/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public float speed =10f;
+	public float minDist = 0.2f;
+
+	private Transform target;
+	private int wavepointIndex = 0;
+
+	void Start(){
+		target = WayPoints.points [0];
+	}
+	void Update (){
+		Vector3 dir = target.position - transform.position;
+		transform.Translate (dir.normalized * speed * Time.deltaTime);
+		if (Vector3.Distance (transform.position, target.position) <= minDist) {
+			GetNextWayPoint ();
+		}
+	}
+	void GetNextWayPoint(){
+		if (wavepointIndex >= WayPoints.points.Length - 1) {
+			EnemyAttack ();
+		} else {
+			wavepointIndex++;
+			target = WayPoints.points [wavepointIndex];
+		}
+	}
+	void EnemyAttack(){
+		Destroy (gameObject);
+	}
+}
---
+++ Assets/scripts/WayPoints.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class WayPoints : MonoBehaviour {
+	public static Transform[] points;
+	void Awake (){
+		points = new Transform[transform.childCount];
+		for (int i = 0; i < points.Length; i++) {
+			points [i] = transform.GetChild (i);
+		}
+	}
+}
