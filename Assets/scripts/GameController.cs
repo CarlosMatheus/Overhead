e57ba8c4356cb4aed9f3951b4b07a492e8f01ae7
chicomcastro <Franciscommcastro@gmail.com>
2017-06-04 00:54:03 -0300

    Finished HP UI
    Done!

--- Assets/scripts/GameController.cs
+++ Assets/scripts/GameController.cs
@@ -38,8 +38,9 @@ public class GameController : MonoBehaviour {
 	public void SetTarget (GameObject _newTarget) {
 		
 		if (currentTarget != null) {
-			// Sets target material (color, specifically) int original one
+			// Sets target material (color in actual one and emission on tempMaterial) into original one
 			currentTarget.GetComponent<MeshRenderer> ().material = currentTarget.GetComponent<TargetSelection> ().originalMaterial;
+			currentTarget.GetComponent<TargetSelection> ().tempMaterial = currentTarget.GetComponent<TargetSelection> ().originalMaterial;
 
 			// If had double clicked the same target, just destarget it
 			if (_newTarget == currentTarget) {
@@ -58,6 +59,7 @@ public class GameController : MonoBehaviour {
 		// Turns targetMaterial color into originalMaterial color and atribute it to targetObject
 		targetMaterial.color = currentTarget.GetComponent<TargetSelection> ().originalMaterial.color;
 		currentTarget.GetComponent<MeshRenderer> ().material = targetMaterial;
+		currentTarget.GetComponent<TargetSelection> ().tempMaterial = targetMaterial;
 
 		// Active HP on top screen
 		enemyHealthBar.SetActive (true);
--- Assets/scripts/TargetSelection.cs
+++ Assets/scripts/TargetSelection.cs
@@ -7,20 +7,46 @@ public class TargetSelection : MonoBehaviour {
 	[Header("To assign")]
 	public float maximumHealth = 100.0f;
 	public GameObject enemyHealthBar;
+	public Material overMaterial;
 
 	[Header("Auto assign (No need to assign)")]
 	public Material originalMaterial;
+	public Material tempMaterial;
 	public float HP;
 
 	void Start () {
+		// Setting current HP to maximumHP
 		HP = maximumHealth;
+
+		// Getting reference for materials to be used later
 		originalMaterial = GetComponent<MeshRenderer> ().material;
+		tempMaterial = GetComponent<MeshRenderer> ().material;
+
+		// Rotating enemy UI (health bar) at beginning
 		enemyHealthBar.transform.rotation = GameObject.FindGameObjectWithTag ("MainCamera").gameObject.transform.rotation;
 	}
 
-	void OnMouseDown ()
+	void OnMouseDown ()  // If this.gameObject had been clicked
 	{
+		// Target it! (logic for untargetting happens on GameController script)
 		GameObject.FindGameObjectWithTag ("GameController").GetComponent<GameController> ().SetTarget (this.gameObject);
+	}
+
+	void OnMouseEnter ()  // If mouse is over this.gameObject
+	{
+		// Gets current material
+		tempMaterial = gameObject.GetComponent<MeshRenderer> ().material;
+
+		// Sets actual material color to overMaterial material
+		overMaterial.color = originalMaterial.color;
+
+		// Sets actual material to overMaterial (with emission, but with originalMaterial color)
+		gameObject.GetComponent<MeshRenderer> ().material = overMaterial;
+	}
+
+	void OnMouseExit ()  // If mouse is not over this.gameObject
+	{
+		gameObject.GetComponent<MeshRenderer> ().material = tempMaterial;
 	}
 
 	void OnTriggerEnter (Collider other)
