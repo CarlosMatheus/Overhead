6328b7515ba991603d1ec03f33c690db3176c3a6
CarlosMatheus <carlosmatheusbs@gmail.com>
2017-06-05 20:28:53 -0300

    Started souls system

---
+++ Assets/SoulsCounter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoulsCounter : MonoBehaviour {
+
+	public float InitialSouls = 200f;
+	public Text soulsText;
+
+	private float souls;
+
+	public void SetSouls(float value){
+		souls = value;
+	}
+
+	public float GetSouls(){
+		return souls;
+	}
+
+	private void Start () {
+		SetSouls (InitialSouls);
+	}
+
+	private void Update(){
+		UpdateUI ();
+	}
+
+	private void UpdateUI(){
+		soulsText.text = Mathf.Round(GetSouls()).ToString();
+	}
+}
