0f3808ea6075bc2c17debb39dea8f09bf8bf2dc8
CarlosMatheus <carlosmatheusbs@gmail.com>
2017-06-04 21:44:36 -0300

    Developed build tower system

---
+++ Assets/scripts/BuildManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour {
+
+	public static BuildManager instance;
+	public GameObject standardTowerPrefab;
+
+	private GameObject towerToBuild;
+
+	public GameObject GetTowerToBuild(){
+		return towerToBuild;
+	}
+
+	private void Awake(){
+		if ( instance != null) {
+			Debug.LogError ("More then one BuildManager in the scene!");
+			return;
+		}
+		instance = this;
+	}
+
+	private void Start(){
+		towerToBuild = standardTowerPrefab;
+	}
+
+}
+
---
+++ Assets/scripts/Node.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Node : MonoBehaviour {
+
+	public Color hoverColor;
+
+	private Renderer rend;
+	private Color startColor;
+	private GameObject tower;
+
+	void Start(){
+		rend = GetComponent<Renderer> ();
+		startColor = rend.material.color;
+	}
+		
+	void OnMouseEnter (){
+		rend.material.color = hoverColor;
+	}
+
+	void OnMouseExit (){
+		rend.material.color = startColor;
+	}
+
+	void OnMouseDown(){
+		if (tower != null) {
+			Debug.Log ("can't build there! - TODO: Display on screen.");
+			return;
+		}
+		GameObject towerToBuild = BuildManager.instance.GetTowerToBuild ();
+		tower = (GameObject)Instantiate (towerToBuild, transform.position, transform.rotation);
+	}
+}
