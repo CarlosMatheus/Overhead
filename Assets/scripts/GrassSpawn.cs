0e38f62bfd31ce522072cc750397e720e5b4c0f1
CarlosMatheus <carlosmatheusbs@gmail.com>
2017-06-05 08:09:33 -0300

    Fixed merge conflicts

--- Assets/scripts/GrassSpawn.cs
+++ Assets/scripts/GrassSpawn.cs
@@ -4,69 +4,45 @@ using UnityEngine;
 
 public class GrassSpawn : MonoBehaviour {
 
-	public GameObject grass00;
-	public GameObject grass01;
-	public GameObject grass02;
-	public GameObject grass03;
-	public GameObject grass04;
-
-	public float chance00;
-	public float chance01;
-	public float chance02;
-	public float chance03;
-	public float chance04;
+	public GameObject[] arr;
+	public float[] chance;
 
 	private float sum;
 	private float val;
 	private float rot;
-
 
 	void Start () {
 		Ajust ();
 		Select ();
 	}
 
+	//Ajust correctly the chances and randoms rotations values
 	void Ajust(){
-		sum = chance00 + chance01 + chance02 + chance03 + chance04;
+		sum = 0;
+		for (int i = 0; i < chance.Length; i++)
+			sum += chance [i];
 		val = Random.value*sum;
 		rot = Mathf.Round (Random.value * 4f);
 	}
 
+	//select randomly a element of the array
 	void Select(){
 		float compVal = 0;
-
-		compVal += chance00;
-		if (val <= chance00) {
-			Spawn (grass00, -90f, 0);
-			return;
-		}
-		compVal += chance01;
-		if (val <= compVal){
-			Spawn (grass01, 0, -0.05f);
-			return;
-		}
-		compVal += chance02;
-		if (val <= compVal){
-			Spawn (grass02, 0, 0);
-			return;
-		}
-		compVal += chance03;
-		if (val <= compVal){
-			Spawn (grass03, 0, 0);
-			return;
-		}
-		compVal += chance04;
-		if (val <= compVal){
-			Spawn (grass04, 0, 0);
-			return;
+		for (int i = 0; i < arr.Length; i++) {
+			compVal += chance[i];
+			if (val <= compVal) {
+				Spawn (i);
+				return;
+			}
 		}
 	}
 
-	void Spawn(GameObject obj, float rot1, float yCorrection){
-		GameObject inst = (GameObject)Instantiate (obj, transform.position, transform.rotation);
-		Vector3 pos = transform.position;
-		inst.transform.rotation = Quaternion.Euler (rot1, 90f * rot, 0);
-		inst.transform.position = new Vector3(pos.x, yCorrection, pos.z);
+	//spawn in a random y rotation, the quaternion need to be converted to vector 3 to oparate it
+	void Spawn(int i){
+		Quaternion rotat = arr [i].transform.rotation;
+		Vector3 rotatInVector = rotat.eulerAngles;
+		rotatInVector = new Vector3 (rotatInVector.x, 90 * rot, rotatInVector.z);
+		GameObject inst = (GameObject)Instantiate (arr [i], transform.position, Quaternion.Euler(rotatInVector));
 		Destroy (gameObject);
 	}
 }
