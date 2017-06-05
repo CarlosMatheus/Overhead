82bc542e927e9cdd8052d93a39e2a2d5ad0ad046
chicomcastro <Franciscommcastro@gmail.com>
2017-06-05 07:32:09 -0300

    A lot of changes
    Att!

---
+++ Assets/scripts/MouseFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseFollow : MonoBehaviour {
+
+	public float distanceFromCamera = 5.0f;
+
+	void Update () {
+		Vector3 mousePosition = Input.mousePosition;
+		mousePosition.z = distanceFromCamera;
+
+		Vector3 mouseScreenToWorld = Camera.main.ScreenToWorldPoint(mousePosition);
+
+		transform.position = mouseScreenToWorld;
+	}
+}
---
+++ Assets/scripts/Teleporter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour {
+
+	private Animator anim;
+
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+
+	public void Teleport () {
+		transform.LookAt (GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerController> ().currentTarget.transform.position);
+
+		anim.SetBool ("start", true);
+	}
+}
