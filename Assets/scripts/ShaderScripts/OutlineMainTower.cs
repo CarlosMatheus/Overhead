5db9f1de940723b7564ca27254394702a8057cf2
HikkusT <luizhenrique98@gmail.com>
2017-06-08 07:43:58 -0300

    Fixed main tower selection outline

---
+++ Assets/scripts/ShaderScripts/OutlineMainTower.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutlineMainTower : MonoBehaviour {
+
+    public GameObject cilinder;
+
+    private void OnMouseEnter()
+    {
+        cilinder.GetComponent<OutlineObjectMainTower>().enabled = true;
+        cilinder.GetComponent<OutlineObjectMainTower>().entrou = true;
+    }
+
+    private void OnMouseExit()
+    {
+        cilinder.GetComponent<OutlineObjectMainTower>().enabled = true;
+        cilinder.GetComponent<OutlineObjectMainTower>().entrou = false;
+    }
+}
---
+++ Assets/scripts/ShaderScripts/OutlineObjectMainTower.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutlineObjectMainTower : MonoBehaviour {
+
+    [SerializeField] public Color GlowColor;
+    public float FadeFactor = 5;
+    public bool entrou = false;
+
+    private Renderer[] _renderers;
+    private List<Material> _materials = new List<Material>();
+    private Color _targetColor;
+    private Color _currentColor;
+
+    void Start()
+    {
+        _renderers = GetComponentsInChildren<Renderer>(); //Gets all the Children Renderers, to get access to their materials
+
+        foreach (Renderer renderer in _renderers)
+        {
+            _materials.AddRange(renderer.materials); //Getting access to their materials
+        }
+    }
+
+
+    void CheckColor()
+    {
+        if (entrou)
+        {
+            Debug.Log("Entrouu");
+            _targetColor = GlowColor; //If the mouse is on the collider, select the color to glow
+            enabled = true;
+        }
+        else
+        {
+            _targetColor = Color.black; //If the mouse left the collider, make the aura black, so the effect won't appear
+            enabled = true;
+        }
+    }
+
+    void Update()
+    {
+        Debug.Log(entrou.ToString());
+        if (entrou)
+        {
+            Debug.Log("Entrouu");
+            _targetColor = GlowColor; //If the mouse is on the collider, select the color to glow
+            enabled = true;
+        }
+        else
+        {
+            _targetColor = Color.black; //If the mouse left the collider, make the aura black, so the effect won't appear
+            enabled = true;
+        }
+        _currentColor = Color.Lerp(_currentColor, _targetColor, Time.deltaTime * FadeFactor); //Just some fade in and out effect
+
+        for (int i = 0; i < _materials.Count; i++)
+        {
+            _materials[i].SetColor("_glowColor", _currentColor); //Changing the color of the aura
+        }
+
+        if (_currentColor.Equals(_targetColor)) //When to stop the fade
+            enabled = false;
+    }
+}
