7ec6b48d6f9108b3ed675890d9605f053b9253bd
HikkusT <luizhenrique98@gmail.com>
2017-06-09 05:43:27 -0300

    Finished Main Menu

---
+++ Assets/scripts/MenuManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+public void LoadScene(int sceneNumber)
+    {
+        SceneManager.LoadScene(sceneNumber);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
