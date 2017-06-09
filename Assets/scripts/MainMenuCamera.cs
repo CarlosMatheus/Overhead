698344fbf2f4927e5ec2544bea864c3f2f2019bc
HikkusT <luizhenrique98@gmail.com>
2017-06-09 04:23:37 -0300

    Main Menu scene almost done

---
+++ Assets/scripts/MainMenuCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuCamera : MonoBehaviour {
+
+    public float speed;
+	
+	
+	void Update () {
+        OrbitAround();
+	}
+
+    void OrbitAround()
+    {
+        transform.RotateAround(new Vector3(0, this.transform.position.y, 0), Vector3.up, speed * Time.deltaTime);
+    }
+}
