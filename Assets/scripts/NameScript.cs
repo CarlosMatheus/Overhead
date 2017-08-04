b01b0ab484ffc1fd92d0f8468c2db424edf4799d
CarlosMatheus <carlosmatheusbs@gmail.com>
2017-08-04 15:23:16 -0300

    Done UI Leader Board Interface

---
+++ Assets/scripts/NameScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NameScript : MonoBehaviour {
+
+	public void GetInput(string name){
+		Debug.Log (name);
+	}
+
+}
