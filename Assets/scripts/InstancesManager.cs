5dff50cce91b84a0403b2b077804f653538fc1de
Carlos Matheus <carlosmatheusbs@gmail.com>
2017-08-12 10:28:38 -0300

    Added new materials and prefabs

---
+++ Assets/scripts/InstancesManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class InstancesManager : MonoBehaviour {
+
+    [SerializeField] private GameObject shopGObj;
+
+    public GameObject GetShopGObj()
+    {
+        return shopGObj;
+    }
+}
