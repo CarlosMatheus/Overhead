cd552a6f4773d38fcb33d364d1c7bdc8810a56f7
HikkusT <luizhenrique98@gmail.com>
2017-08-15 14:28:23 -0300

    Added Bird Interaction

---
+++ Assets/scripts/BirdScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour {
+
+    //public Vector3 destPoint = Vector3.zero;
+
+    public Vector3 direction = Vector3.zero;
+    public float speed = 0.05f;
+
+	// Use this for initialization
+	void Start () {
+        //direction = destPoint - transform.position;
+        Destroy(gameObject, 90);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 movement = speed * direction * Time.deltaTime;
+        transform.position = transform.position + movement;
+	}
+}
