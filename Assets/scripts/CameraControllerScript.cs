c8109908119f03a489f45b6c34cc85fa5d3912fa
Carlos Matheus <carlosmatheusbs@gmail.com>
2017-09-07 20:55:56 -0300

    Fixed camera script

--- Assets/scripts/CameraControllerScript.cs
+++ Assets/scripts/CameraControllerScript.cs
@@ -14,14 +14,17 @@ public class CameraControllerScript : MonoBehaviour {
 	private float maxX = 80f;
 	private float minZ = 10f;
 	private float maxZ = 80f;
+    private float epsilon = 0.0001f;
 
-	void Update () {
+	private void Update ()
+    {
 		MoveScreen ();
 		ZoomScroll ();
 		LimitPosition ();
 	}
 
-	private void Start(){
+	private void Start()
+    {
         minX = (-1f) * moduleDimension - 8f;
         maxX = moduleDimension - 10f;
 		minZ = (-1f)*moduleDimension - 17f;
@@ -29,55 +32,105 @@ public class CameraControllerScript : MonoBehaviour {
 	}
 
 	//move the camera white awsd or with mouse in the border
-	void MoveScreen(){
-		if ( Input.GetKey ("w") || 
-			( Input.mousePosition.y >= (Screen.height - panBoardThickness) && Input.mousePosition.y <= Screen.height)
-			|| Input.GetKey(KeyCode.UpArrow)) {
+	void MoveScreen()
+    {
+		if ( 
+            Input.GetKey ("w") || 
+			( Input.mousePosition.y >= (Screen.height - panBoardThickness) && 
+             Input.mousePosition.y <= Screen.height)
+			|| Input.GetKey(KeyCode.UpArrow)
+           ) 
+        {
 			GoForward ();
 		}
 		if ( Input.GetKey ("s") || 
-			( Input.mousePosition.y <= panBoardThickness && Input.mousePosition.y >= 0f ) ||
-			Input.GetKey(KeyCode.DownArrow)) {
+			( Input.mousePosition.y <= panBoardThickness && 
+             Input.mousePosition.y >= 0f ) ||
+			Input.GetKey(KeyCode.DownArrow)
+           ) 
+        {
 			GoBack ();
 		}
-		if ( Input.GetKey ("d") || 
+		if ( 
+            Input.GetKey ("d") || 
 			( Input.mousePosition.x >= Screen.width - panBoardThickness && Input.mousePosition.x <= Screen.width) ||
-			Input.GetKey(KeyCode.RightArrow)) {
+			Input.GetKey(KeyCode.RightArrow)
+           ) 
+        {
 			GoRight ();
 		}
-		if (Input.GetKey ("a") || 
+		if (
+            Input.GetKey ("a") || 
 			(Input.mousePosition.x <= panBoardThickness && Input.mousePosition.x >= 0f) ||
-			Input.GetKey(KeyCode.LeftArrow)) {
+			Input.GetKey(KeyCode.LeftArrow)
+        ) 
+        {
 			GoLeft ();
 		}
 	}
 
 	//Transform mouse scroll into zoom and defines min and max distances
-	void ZoomScroll(){
-		float scroll = Input.GetAxis ("Mouse ScrollWheel");
-		//limit de scrollMaxSensibility
-		scroll = Mathf.Clamp (scroll, (-1f)*scrollMaxSensibility, scrollMaxSensibility);
+	void ZoomScroll()
+    {
+        float scroll = LimitScrollSensibility( Input.GetAxis("Mouse ScrollWheel") );
 
-		ZoomVerticalMoviment (scroll);
-		ZoomHorizontalMoviment (scroll);
+        if ( transform.position.y > minY - epsilon && transform.position.y < maxY + epsilon )
+            ZoomMoviment(scroll);
 	}
 
-	//Camera vetical moviment
-	void ZoomVerticalMoviment(float scroll){
-		Vector3 pos = transform.position;
-		pos.y -= scroll * scrollSpeed * 200 * Time.deltaTime;
-		transform.position = pos;
+    private float LimitScrollSensibility(float scroll)
+    {
+        return Mathf.Clamp(scroll, (-1f) * scrollMaxSensibility, scrollMaxSensibility);
+    }
+
+	//Camera zoom moviment
+	void ZoomMoviment(float scroll)
+    {
+		Vector3 triedPosition = transform.position;
+		triedPosition.y -= scroll * scrollSpeed * 200 * Time.deltaTime;
+
+        if ( triedPosition.y > minY - epsilon && triedPosition.y < maxY + epsilon )
+        {
+            ZoomVerticalMoviment(triedPosition);
+            ZoomHorizontalMoviment(scroll);
+            return;
+        }
+        if ( triedPosition.y <= minY - epsilon )
+        {
+            float triedMoviment = Mathf.Abs(transform.position.y - triedPosition.y );
+            float maxDisplacement = Mathf.Abs(transform.position.y - (minY - epsilon));
+            float movimentRation = maxDisplacement / triedMoviment;
+            triedPosition.y = minY;
+            ZoomVerticalMoviment( triedPosition );
+            ZoomHorizontalMoviment( scroll * movimentRation );
+            return;
+        }
+        if (triedPosition.y >= maxY + epsilon)
+        {
+            float triedMoviment = Mathf.Abs( transform.position.y - triedPosition.y );
+            float maxDisplacement = Mathf.Abs( transform.position.y - (maxY + epsilon) );
+            float movimentRation = maxDisplacement / triedMoviment;
+            triedPosition.y = maxY;
+            ZoomVerticalMoviment( triedPosition );
+            ZoomHorizontalMoviment(scroll * movimentRation);
+            return;
+        }
 	}
 
-	//this makes the camera go forward only if it is not in the heights limits
-	void ZoomHorizontalMoviment(float scroll){
+    void ZoomVerticalMoviment(Vector3 pos)
+    {
+        transform.position = pos;
+    }
+
+	void ZoomHorizontalMoviment(float scroll)
+    {
 		Vector3 pos = transform.position;
-		if ( pos.y > minY+0.01f && pos.y < maxY-0.01f )
-			transform.Translate (Vector3.forward * scroll * scrollSpeed * 200 * Time.deltaTime, Space.Self);
+		transform.Translate (Vector3.forward * scroll * scrollSpeed * 200 * Time.deltaTime, Space.Self);
 	}
 
 	//limit camera position
-	void LimitPosition(){
+	void LimitPosition()
+    {
 		float posX = transform.position.x;
 		float posY = transform.position.y;
 		float posZ = transform.position.z;
@@ -89,19 +142,23 @@ public class CameraControllerScript : MonoBehaviour {
 		transform.position = new Vector3 (posX, posY, posZ);
 	}
 
-	public void GoForward () {
+	public void GoForward () 
+    {
 		transform.Translate ( Vector3.forward * panSpeed * Time.deltaTime, Space.Self );
 	}
 
-	public void GoBack () {
+	public void GoBack () 
+    {
 		transform.Translate ( Vector3.back * panSpeed * Time.deltaTime, Space.Self );
 	}
 
-	public void GoLeft () {
+	public void GoLeft () 
+    {
 		transform.Translate ( Vector3.left * panSpeed * Time.deltaTime, Space.Self );
 	}
 
-	public void GoRight () {
+	public void GoRight () 
+    {
 		transform.Translate ( Vector3.right * panSpeed * Time.deltaTime, Space.Self );
 	}
 }
